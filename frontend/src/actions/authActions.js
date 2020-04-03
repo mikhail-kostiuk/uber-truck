c5ede1b751ec94849eca9cdb87b349c54d8d3bf4
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2020-04-03 16:14:03 +0300

    Refactor login/logout code to respected functions

--- frontend/src/actions/authActions.js
+++ frontend/src/actions/authActions.js
@@ -1,68 +1,92 @@
-import axios from "axios";
-import jwt_decode from "jwt-decode";
-import setAuthToken from "../utils/setAuthToken";
-import * as actionTypes from "./actionTypes";
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
+import * as actionTypes from './actionTypes';
 
 export const registerUser = (userData, history) => {
-  return dispatch => {
+  return (dispatch) => {
     axios
-      .post("http://localhost:5050/api/users/register", userData)
-      .then(res => history.push("/login"))
-      .catch(err => {
-        console.log(err);
+      .post('/api/users/register', userData)
+      .then((res) => {
+        const {token} = res.data;
+
+        login(token, history, dispatch);
+      })
+      .catch((err) => {
         return dispatch({
           type: actionTypes.SET_ERROR,
-          payload: err.response.data,
+          payload: err.response.data.error,
         });
       });
   };
 };
 
-export const loginUser = userData => {
-  return dispatch => {
-    console.log(userData);
+export const loginUser = (userData, history) => {
+  return (dispatch) => {
     axios
-      .post("http://localhost:5050/api/users/login", userData)
-      .then(res => {
-        // Save token to localStorage
-        const { token } = res.data;
-        localStorage.setItem("jwtToken", token);
+      .post('/api/users/login', userData)
+      .then((res) => {
+        const {token} = res.data;
 
-        // Set token to Auth header
-        setAuthToken(token);
-
-        // Decode token to get user data
-        const decodedUserData = jwt_decode(token);
-
-        // Set current user
-        dispatch({
-          type: actionTypes.SET_CURRENT_USER,
-          payload: decodedUserData,
-        });
+        login(token, history, dispatch);
       })
-      .catch(err =>
-        dispatch({
+      .catch((err) => {
+        return dispatch({
           type: actionTypes.SET_ERROR,
-          payload: err.response.data,
-        })
-      );
+          payload: err.response.data.error,
+        });
+      });
   };
 };
 
 export const logoutUser = () => {
-  return dispatch => {
-    // Remove token from local storage
-    localStorage.removeItem("jwtToken");
-
-    // Remove auth header for future requests
-    setAuthToken(false);
-
-    window.location.href = "/login";
-
-    // Set current user to empty object
-    dispatch({
-      type: actionTypes.SET_CURRENT_USER,
-      payload: {},
-    });
+  return (dispatch) => {
+    logout(dispatch);
   };
 };
+
+export const deleteUser = (id) => {
+  return (dispatch) => {
+    axios
+      .delete(`/api/users/${id}`)
+      .then((res) => {
+        logout(dispatch);
+      })
+      .catch((err) => {
+        return dispatch({
+          type: actionTypes.SET_ERROR,
+          payload: err.response.data.error,
+        });
+      });
+  };
+};
+
+function login(token, history, dispatch) {
+  localStorage.setItem('jwtToken', token);
+
+  // Set token to Auth header
+  setAuthToken(token);
+
+  const decodedUserData = jwtDecode(token);
+
+  dispatch({
+    type: actionTypes.SET_CURRENT_USER,
+    payload: decodedUserData,
+  });
+
+  history.push('/');
+}
+
+function logout(dispatch) {
+  localStorage.removeItem('jwtToken');
+
+  // Remove auth header for future requests
+  setAuthToken(false);
+
+  window.location.href = '/login';
+
+  dispatch({
+    type: actionTypes.SET_CURRENT_USER,
+    payload: {},
+  });
+}

4523ab88bcb7dbafee662f35b87bccd2d3817655
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2020-04-03 16:07:24 +0300

    Add login route

--- frontend/src/routes/Login/Login.js
+++ frontend/src/routes/Login/Login.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Login.scss';
 import Header from '../../components/Header/Header';
+import LoginForm from '../../components/Forms/LoginForm';
 
-function Login() {
+function Login(props) {
   return (
     <div className="login">
       <Header />
+      <div className="login__form">
+        <LoginForm history={props.history} />
+      </div>
     </div>
   );
 }
